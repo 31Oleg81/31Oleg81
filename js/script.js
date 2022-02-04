071054a463c8afbb0e544ef078fdb57a0cd7d301
31Oleg81 <cot.100@mail.ru>
2022-02-04 14:26:47 +0300

    use functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 "use strict";
-const numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+		numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+	}
+}
+
+start();
+
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -8,18 +19,25 @@ const personalMovieDB = {
 	privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-	const a = prompt('Один из последних просмотренных фильмов?', ''),
-		  b = prompt('На сколько оцените его?', '');
 
-	if (a != null && b != null && a != '' && b != '' && a.length < 50){
-		personalMovieDB.movies[a] = b;
-		console.log('done');
-	} else {
-		console.log('error');
-		i--;
-	}
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('Один из последних просмотренных фильмов?', ''),
+			  b = prompt('На сколько оцените его?', '');
+	
+		if (a != null && b != null && a != '' && b != '' && a.length < 50){
+			personalMovieDB.movies[a] = b;
+			console.log('done');
+		} else {
+			console.log('error');
+			i--;
+		}
+	}	
+}
+rememberMyFilms();
 
+
+function detectPersonalLevel() {
 	if (personalMovieDB.count < 10){
 		console.log('Просмотренно мало фильмов');
 	} else if (personalMovieDB.count >= 10 && personalMovieDB.count <30){
@@ -29,8 +47,23 @@ for (let i = 0; i < 2; i++) {
 	} else {
 		console.log('Произошла ошибка');
 	}
+}	
+detectPersonalLevel();
 
-	
+function showMyDB (hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
 }
 
-console.log(personalMovieDB);
+showMyDB (personalMovieDB.privat);
+
+function writeYourGenries() {
+	for (let i = 1; i <=3; i++) {
+		const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+		personalMovieDB.genres[i - 1] = genre;
+	}
+}
+
+writeYourGenries();
+
